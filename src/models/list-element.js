e068203049a98ef0ce8e423a4e656a367e0e6de9
testinnplayin <archaurore@gmail.com>
2019-12-17 10:09:06 +0100

    made a basic list element schema

---
+++ src/models/list-element.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ListElementSchema = Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    prop1 : String,
+    prop2 : Number,
+    prop3 : Boolean,
+    prop4 : String
+});
+
+const ListElement = mongoose.model("ListElement", ListElementSchema);
+
+module.exports = {
+    ListElement,
+    ListElementSchema
+};
